44aa12644e918a1d42d7c850222f077bfa4198cd
andy910829 <71509005+andy910829@users.noreply.github.com>
2023-07-12 22:09:13 +0800

    Delete detector_backend directory

--- detector_backend/userInit.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-class userInit:
-    def __init__(self):
-        self.client = pymongo.MongoClient('localhost', 27017)
-        self.db = self.client['user']
-        self.collection = self.db['userInfo']
-
-    
